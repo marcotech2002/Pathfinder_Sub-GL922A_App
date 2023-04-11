ed0c7c7451b0cedce5ea7c0da216466cb0adc09d
prqsVin <marcos.rocha@sou.unaerp.edu.br>
2023-04-11 10:23:20 -0300

    commit tela cadastro

--- projeto_app/lib/view/tela_cadastro.dart
+++ projeto_app/lib/view/tela_cadastro.dart
@@ -2,6 +2,8 @@
 
 import 'package:flutter/material.dart';
 
+import '../style/palette.dart';
+
 class TelaCadastro extends StatefulWidget 
 {
   const TelaCadastro({super.key});
@@ -12,11 +14,107 @@ class TelaCadastro extends StatefulWidget
 
 class _TelaCadastroState extends State<TelaCadastro> 
 {
-  
-
   @override
   Widget build(BuildContext context) 
   {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Palette().Primary,
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Image.asset('assets/images/logo.png'),
+            Text(
+              "Cadastro",
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontSize: 50,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(100,0,100,0),
+              child: Column(
+                children: [
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      labelStyle: const TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 25,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Nome',
+                      labelStyle: const TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 25,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Senha',
+                      labelStyle: const TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Confirmar Senha',
+                        labelStyle: const TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ),
+                ]
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0,50,0,50),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Palette().Secondary,
+                    ),
+                    onPressed: () {},
+                    child: Text(
+                      'Confirmar',
+                      style: const TextStyle(
+                        fontSize: 25,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 100,
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Palette().Secondary,
+                    ),
+                    onPressed: () {},
+                    child: Text(
+                      'Cancelar',
+                      style: const TextStyle(
+                        fontSize: 25,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }

624eefaff821ea6682724ec096601466770b521f
prqsVin <marcos.rocha@sou.unaerp.edu.br>
2023-04-18 11:14:23 -0300

    splash

---
+++ projeto_app/lib/view/tela_splash.dart
@@ -0,0 +1,30 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:easy_splash_screen/easy_splash_screen.dart';
+import 'package:flutter/material.dart';
+
+import 'package:projeto_app/view/tela_login.dart';
+
+import '../style/font.dart';
+import '../style/palette.dart';
+
+class TelaSplash extends StatelessWidget {
+  const TelaSplash({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return EasySplashScreen(
+      logo: Image.network('assets/images/logo.png'),
+      logoWidth: 200,
+      backgroundColor: Palette().Primary,
+      showLoader: true,
+      loaderColor: Palette().Tertiary,
+      loadingText: Text(
+        'Carregando...',
+        style: Font().NormalFont,
+      ),
+      durationInSeconds: 5,
+      navigator: TelaLogin(),
+    );
+  }
+}
